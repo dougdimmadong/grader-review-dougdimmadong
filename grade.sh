70258df16638e6ac24f3e32dd097c49da5aaaa3c
dougdimmadong <kylepon73@gmail.com>
2023-11-06 13:50:59 -0800

    scripting edits

--- grade.sh
+++ grade.sh
@@ -8,7 +8,27 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+file_path="./student-submission"
 
+if [ -e "$file_path" ]; then
+    echo "File exists at $file_path"
+else
+    echo "File does not exist at $file_path"
+    exit 1
+fi
+
+cp ./student-submission/ListExamples.java ./grading-area
+cp ./TestListExamples.java ./grading-area
+
+cd ./grading-area
+
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
+java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > ./junitOutput.txt
+if [ $? -eq 0 ]; then
+    echo Success
+else
+    echo Failure
+fi
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
